1a042879b4d19ae8a53cb587a432edb85d15bfa2
Edson <edson.fullstack@gmail.com>
2019-11-28 16:21:55 -0300

    flask api working

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
+from flask import Flask
 def fatorial(numero):
     if numero in (0, 1):
         return 1
     return numero * fatorial(numero - 1)
+    
+app = Flask(__name__)
+
+#home page
+@app.route("/")
+def cinnecta():
+    return "Hello World"
+
+if __name__=="__main__":
+    app.run()
