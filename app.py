0227e498aed89f5ce5b78b85efdaf0ca13d9bc49
Edson <edson.fullstack@gmail.com>
2019-11-29 11:27:29 -0300

    iterpolação de codigos

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask , render_template
 
 #esta varialvel e funcão controlam o debug passo a passo no ambiente de testes
-TEST_CONTROL=True
-def tests(mensagem):
+TEST_CONTROL=1
+def tests(controle,mensagem):
     """mensagem a ser exibida no durante testes"""
-    if(TEST_CONTROL):
+    if(TEST_CONTROL>=controle):
         print(str(mensagem))
 #remove caracteres e retorna toda a string em lower case
 def remover_caracteres(old_string, to_remove):
@@ -20,111 +20,79 @@ def substituir_caracteres(old_string, to_remove,to_replace):
         new_string = new_string.replace(i, to_replace)
     return new_string.lower()
 
-#!fora de ordem porem 
+
+
+#conta os elementos e armazena em um dicionario 
+#!utiliza como indice uma concatenação entre o indice do vetor e o item ao qual se esta procurando
+def contar(vetores,vocabulario,gramatica):
+    dicionario=dict()
+    if(gramatica=='gram1'):
+        for i in range(len(vetores)):
+            for item in vocabulario:
+                dicionario['texto'+str(i+1)+vocabulario[item]]=vetores[i].count(vocabulario[item])
+    if(gramatica=='gram2'):
+        for i in range(len(vetores)):
+            for item in range(len(vetores[i])-1):
+                analise=vetores[i][item]+' '+vetores[i][item+1]
+                #print(vocabulario)
+                for j in vocabulario:
+                    print(vocabulario[j])
+                    if(vocabulario[j]==analise):
+                        key={vetores[i][item]+vetores[i][item+1]}
+                        dicionario[key]+=1
+
+                tests(1,analise)
+    
+    tests(1,'Dicionario:'+str(dicionario)) 
+    return 
+
 #a partir do vetor contendo os textos retorna o vocabulario e os vetores contendo as palavras da frase
-def gerar_listas_simples(textos,stop_words):
+def gerar_listas_simples(textos,stop_words,gramatica):
     apagar_caracteres=',.!?-_%&#'
     textoCompleto=str()
     vector={}
-    tests(textos)
+    tests(1,textos)
+    vocabulario_set=set()
     vocabulario={}
     j=0
     for i in range(len(textos)):
         #remove caracteres e tratamento de texto
         vector[i]=remover_caracteres(textos[i],apagar_caracteres).split(' ')
         #j representa o indice para a lista de vocabulario
-        tests(vector[i])
-        for item in range(len(vector[i])):
+        #retirar elementos vazios do vetor
+        while('' in vector[i]):
+            vector[i].remove('')
+        if(gramatica=="gram1"):
+            alterar=0
+        if(gramatica=="gram2"):
+            alterar=-1
+        for item in range(len(vector[i])+alterar):
+            if(gramatica=="gram1"):
+                analise=vector[i][item]
+            if(gramatica=="gram2"):
+                analise=vector[i][item]+' '+vector[i][item+1]
+            tests(2,'Analise:'+analise)
             #tratamento para eliminar stopwords e adicionar os elementos que nao se repetem
-            if (vector[i][item] in stop_words):
-                tests('Remove(stopword):'+vector[i][item])
+            if (analise in stop_words):
+                tests(2,'Remove(stopword):'+analise)
                 continue
-            if(vector[i][item] in vocabulario):
-                tests('Remove(na lista):'+vector[i][item])
-                continue
-            else:
-                tests('Add:'+vector[i][item])
-                vocabulario[j]=vector[i][item]
+            elif (analise not in vocabulario_set):
+                tests(2,'Add:'+analise)
+                vocabulario_set.add(analise)
+                vocabulario[j]=analise
                 j=j+1
-    tests('Vocabulario '+str(len(vocabulario))+':'+str(vocabulario))
-    
-    #return vocabulario,vector
-def gerar_listas_2gram(textos,stop_words):
-    apagar_caracteres=',.!?-_%&#'
-    textoCompleto=str()
-    vector={}
-    tests(textos)
-    vocabulario={}
-    for i in range(len(textos)):
-        vector[i]=remover_caracteres(textos[i],apagar_caracteres).split(' ')
-        j=0
-        for item in range(len(vector[i])):
-            if (vector[i][item] in stop_words):
-                tests('Remove:'+vector[i][item])
-                continue
             else:
-                tests('Add:'+vector[i][item])
-                vocabulario[j]=vector[i][item]
-                j+=1
-    tests('Vocabulario '+str(len(vocabulario))+':'+str(vocabulario))
+                tests(2,'Remove:'+vector[i][item])
+    tests(1,'Vocabulario'+gramatica+'['+str(len(vocabulario))+']:'+str(vocabulario))
+    
+    return vocabulario,vector
 
-    #return vocabulario,vector
-
-#conta os elementos e armazena em um dicionario 
-#!utiliza como indice uma concatenação entre o indice do vetor e o item ao qual se esta procurando
-def contar_e_ignorar(vetores,listaPalavras,stop_words):
-    dic=dict()
-    listas={}
-    for i in range(len(vetores)):
-        for item in listaPalavras:
-            #ignora palavras inseridas no vetor de StopWords
-            if item in stop_words:
-                continue
-            dic[str(i)+str(item)]=vetores[i].count(item)
-            #print pra facilitar a vizualização dos elementos em produção
-            #print(str(vetores[i])+'palavra:'+item+str(vetores[i].count(item)))
-    return dic
-
-#Formata a saida para mostra o vocabulario e os vetores de incidencia
-def formatar_saida(listaPalavras,vetores,lista):
-    #dicionario que armazenara o retorno
-    dic=dict()
-    resultLista=''
-    i=1
-    for item in listaPalavras:
-        resultLista=resultLista+str(i)+'.'+item+' '
-        i+=1
-    i=0
-    dic['vocabulario']=resultLista
-    for i in range(len(vetores)):
-        #print pra facilitar a vizualização dos elementos em produção
-        #print('Vetor De Incidencia['+str(i)+']:'+str(vetores[i]))
-        index='texto'+str(i+1)
-        retorno=index+' :['
-        j=0
-        for item in listaPalavras:
-            retorno=retorno+str(lista[str(i)+str(item)])
-            j+=1
-            if(j < len(listaPalavras)):
-                retorno=retorno+','
-        retorno=retorno+']'
-        dic[index]=retorno
-    return dic
 #função principal para execução da primeira parte do exercicio
-def processar_texto(textos,stop_words):
-    #listaPalavras,vector=
-    gerar_listas_simples(textos,stop_words)
+def processar_texto(textos,stop_words,gramatica):
+    vocabulario,vector=gerar_listas_simples(textos,stop_words,gramatica)
     #gerar teste com resultados da parte 1// 
-    #contagem=Contar_E_Ignorar(vector,listaPalavras,stop_words)
-    #formatado=Formatar_Saida(listaPalavras,vector,contagem)
-    #return formatado 
-def processar_texto2(textos,stop_words):
-    #listaPalavras,vector=
-    #gerar_listas_2gram(textos,stop_words)
-    #gerar teste com resultados da parte 1// 
-    #contagem=Contar_E_Ignorar(vector,listaPalavras,stop_words)
-    #formatado=Formatar_Saida(listaPalavras,vector,contagem)
-    return  
+    contagem=contar(vector,vocabulario,gramatica)
+    return 
 
     
 myapp = Flask(__name__)
@@ -143,9 +111,9 @@ def gramatica1():
     e=['Falar é fácil. Mostre-me o código.','É fácil escrever código. Difícil é escrever código que funcione.']
     #StopWords adicionadas em um vetor
     stop_words='',''
-    vocabulario=processar_texto(e,stop_words)
+    vocabulario=processar_texto(e,stop_words,'gram1')
 
-    return render_template('lista.html',format=vocabulario)
+    return render_template('lista.html')
         
 @myapp.route("/gram2")
 def gramatica2():
@@ -153,9 +121,9 @@ def gramatica2():
     e=['Falar é fácil. Mostre-me o código.','É fácil escrever código. Difícil é escrever código que funcione.']
     #StopWords adicionadas em um vetor
     stop_words='',''
-    vocabulario=processar_texto2(e,stop_words)
+    vocabulario=processar_texto(e,stop_words,'gram2')
 
-    return render_template('lista.html',format=vocabulario)
+    return render_template('lista.html')
 
 
 if __name__=="__main__":
